5c85cd14d9e675060454480563b9fbb0a04b1af1
Pooja <151643979+Poojadurgam10@users.noreply.github.com>
2024-01-24 18:41:08 +0530

    Add files via upload

---
+++ pycls.py
@@ -0,0 +1,36 @@
+Python 3.11.0rc2 (main, Sep 11 2022, 20:22:52) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> pow(2,4)
+16
+>>> import math
+>>> math.sqrt(81)
+9.0
+>>> print(math.pi)
+3.141592653589793
+>>> print(math.pow(9))
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(math.pow(9))
+TypeError: pow expected 2 arguments, got 1
+>>> print(math.pow(2,8))
+256.0
+>>> print(math.pow(7,3))
+343.0
+>>> 7**7
+823543
+>>> 2**3
+8
+>>> 3**5
+243
+>>> import math as m
+>>> m.pow(6,3)
+216.0
+>>> from math import sqrt
+>>> sqrt(343)
+18.520259177452136
+>>> sqrt(156)
+12.489995996796797
+>>> sqrt(152)
+12.328828005937952
+>>> sqrt(144)
+12.0
